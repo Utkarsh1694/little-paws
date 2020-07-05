b1df358a90b34e27f7d7ea153e3e14f200114e73
nupurrajal <nupurrajal@gmail.com>
2020-07-05 14:31:39 +0530

    home.html

--- src/main/java/com/pet/logincontroller/LoginController.java
+++ src/main/java/com/pet/logincontroller/LoginController.java
@@ -5,6 +5,7 @@ import com.pet.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -35,13 +36,13 @@ public class LoginController {
     }
 
 
-        @RequestMapping({"/login", "login"})
+        @RequestMapping(value = {"/login", "login"}, method = RequestMethod.POST)
         public String login () {
 
             //check if user creds are valid or not
 
 
-            return "login";
+            return "home";
         }
 
     }
